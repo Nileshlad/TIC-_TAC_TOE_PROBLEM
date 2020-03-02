a3e0e809e8a6c015ea4b422d191ef3bc6de62a43
Nileshlad <ladnilesh1111@gmail.com>
2020-03-02 10:02:19 +0530

    ADDED: TO CHOOSE THE VALID CELLS

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -4,25 +4,73 @@ echo "-------------------------------------------------WELL COME TO TIC TAC TOE 
 
 #CONSTANT
 PLAYER=0
-COMPUTER=1
+COMPUTER=0
+POSSITION=9
 
 #TO DECLARE DICTIONARY
-declare -a gameBord
-   gameBord=(0,0,0,0,0,0,0,0,0)
+declare -a gameBoard 
+
+function resettingBoard(){
+	for (( index=0; index<=$POSSITION; index++ ))
+	do
+		board[$index]=$index
+	done
+}
+#TO FUNCTION CALL
+resettingBoard
 
 #TO CHECK THE SYMBOL 'X' AND '0'
 assingLatter=$((RANDOM%2))
 
 #TO CHECK WHO'S FIRST PLAY
-function CheckForFirst (){
-	toss=$assingLatter
-		if [ $toss -eq 0 ]
-			then
-				echo "The game symbol in 'X'"
-				echo "The plays first in player :$PLAYER"
-			else
-				echo "The game symbol in '0'"
-				echo "The plays first in computer :$COMPUTER"
+function checkForFirst (){
+   toss=$assingLatter
+      if [ $toss -eq 1 ]
+      then
+			PLAYER="X"
+      else
+			PLAYER="O"
+      fi
+}
+#TO FUNCTION CALL
+checkForFirst
+
+#TO DISPLAY GAME BORD
+function displayBoard(){
+
+	echo "--- --- ---"
+	echo "| ${board[1]} | ${board[2]} | ${board[3]} |"
+
+	echo "--- --- ---"
+   echo "| ${board[4]} | ${board[5]} | ${board[6]} |"
+
+	echo "--- --- ---"
+   echo "| ${board[7]} | ${board[8]} | ${board[9]} |"
+}
+
+#TO VALIDATION
+function validation (){
+	displayBoard
+	count=0
+	read -p "Enter the number between 1 to 9 " POSSITION
+	for (( index=0; index<=$POSSITION; index++ ))
+	do
+		if [ $POSSITION -eq $index ]
+		then
+			count=1
+			break
+		else
+			count=0
+		fi
+	done
+		if [ $count -eq 1 ]
+		then
+			board[$index]=X
+			displayBoard ${board[$index]}
+		else
+			validation
 		fi
 }
-CheckForFirst
+
+#TO FUNCTION CALL
+validation
