28369544bf1f2171be159ec3617b335aa1eb9a2e
Nileshlad <ladnilesh1111@gmail.com>
2020-03-02 09:37:32 +0530

    ADDED: TO CHECK THE PLAYER X AND O LATTER

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash -x
 
 echo "-------------------------------------------------WELL COME TO TIC TAC TOE GAME------------------------------------------"
@@ -6,3 +5,13 @@ echo "-------------------------------------------------WELL COME TO TIC TAC TOE 
 #TO DECLARE DICTIONARY
 declare -a gameBoard
 	gameBord=(0,0,0,0,0,0,0,0,0)
+
+#TO CHECK THE SYMBOL 'X' AND '0'
+assingLatter=$((RANDOM%2))
+
+if [ $assingLatter -eq 0 ]
+then
+		echo "The game symbol in 'X'"
+else
+		echo "The game symbol in '0'"
+fi
