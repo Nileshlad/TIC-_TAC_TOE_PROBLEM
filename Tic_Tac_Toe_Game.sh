620f986b6ebce71353e3d2f71f67ec73588ed38d
Nileshlad <ladnilesh1111@gmail.com>
2020-03-02 09:43:57 +0530

    ADDED: TO CHECK THE PLAY FIRST

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -2,16 +2,27 @@
 
 echo "-------------------------------------------------WELL COME TO TIC TAC TOE GAME------------------------------------------"
 
+#CONSTANT
+PLAYER=0
+COMPUTER=1
+
 #TO DECLARE DICTIONARY
-declare -a gameBoard
-	gameBord=(0,0,0,0,0,0,0,0,0)
+declare -a gameBord
+   gameBord=(0,0,0,0,0,0,0,0,0)
 
 #TO CHECK THE SYMBOL 'X' AND '0'
 assingLatter=$((RANDOM%2))
 
-if [ $assingLatter -eq 0 ]
-then
-		echo "The game symbol in 'X'"
-else
-		echo "The game symbol in '0'"
-fi
+#TO CHECK WHO'S FIRST PLAY
+function CheckForFirst (){
+	toss=$assingLatter
+		if [ $toss -eq 0 ]
+			then
+				echo "The game symbol in 'X'"
+				echo "The plays first in player :$PLAYER"
+			else
+				echo "The game symbol in '0'"
+				echo "The plays first in computer :$COMPUTER"
+		fi
+}
+CheckForFirst
