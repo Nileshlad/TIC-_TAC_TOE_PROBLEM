ea1555c1fe0cab2d516e5d8524d53d1373bf0c46
Nileshlad <ladnilesh1111@gmail.com>
2020-03-02 10:10:27 +0530

    MODIFIED: TO PROGRAM CASE STATEMENT SQUENCE

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -42,10 +42,10 @@ function displayBoard(){
 	echo "| ${board[1]} | ${board[2]} | ${board[3]} |"
 
 	echo "--- --- ---"
-   echo "| ${board[4]} | ${board[5]} | ${board[6]} |"
+	echo "| ${board[4]} | ${board[5]} | ${board[6]} |"
 
 	echo "--- --- ---"
-   echo "| ${board[7]} | ${board[8]} | ${board[9]} |"
+	echo "| ${board[7]} | ${board[8]} | ${board[9]} |"
 }
 
 #TO VALIDATION
