a8555c077f61ffcc51664c4cb46989f2a30e7043
Nileshlad <ladnilesh1111@gmail.com>
2020-03-02 09:25:44 +0530

    ADDED: AS THE PLAYER WOULD LIKE TO START FRESH WITH RESETTING THE BOARD

---
+++ Tic_Tac_Toe_Game.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash -x
+
+echo "-------------------------------------------------WELL COME TO TIC TAC TOE GAME------------------------------------------"
+
+#TO DECLARE DICTIONARY
+declare -a gameBord
+	gameBord=(0,0,0,0,0,0,0,0,0)
