a12cb9a09d8f9320294a98fd9ab302716922dd68
Nileshlad <ladnilesh1111@gmail.com>
2020-03-02 10:55:29 +0530

    ADDED: TO CHECK THE WINNER OR TIE AND THE TURN

--- Tic_Tac_Toe_Game.sh
+++ Tic_Tac_Toe_Game.sh
@@ -2,75 +2,126 @@
 
 echo "-------------------------------------------------WELL COME TO TIC TAC TOE GAME------------------------------------------"
 
-#CONSTANT
-PLAYER=0
-COMPUTER=0
-POSSITION=9
+#DECLEAR DICTIONARY
+declare -a gameBoard
 
-#TO DECLARE DICTIONARY
-declare -a gameBoard 
+#VARIABLE
+playerSymbol=0
+computerSymbol=0
 
+#TO GAME BOARD SIZE
 function resettingBoard(){
-	for (( index=0; index<=$POSSITION; index++ ))
+	for(( index=1; index<=9; index++ ))
 	do
-		board[$index]=$index
+			gameBoard[$index]="$index"
 	done
 }
-#TO FUNCTION CALL
-resettingBoard
 
-#TO CHECK THE SYMBOL 'X' AND '0'
-assingLatter=$((RANDOM%2))
-
-#TO CHECK WHO'S FIRST PLAY
-function checkForFirst (){
-   toss=$assingLatter
-      if [ $toss -eq 1 ]
-      then
-			PLAYER="X"
-      else
-			PLAYER="O"
-      fi
+#TO GAME USE THE SYMBOL
+function assignLatter(){
+	assignLatter=$((RANDOM%2))
+	if [ $assignLatter -eq 1 ]
+	then
+			playerSymbol=X
+	else
+			computerSymbol=O
+	fi
 }
-#TO FUNCTION CALL
-checkForFirst
 
-#TO DISPLAY GAME BORD
+#TO DISPLAY BOARD
 function displayBoard(){
+	local index=1
+	for (( indexOne=0; indexOne<3; indexOne++ ))
+	do
+			echo "|---|---|---|"
+			echo "| "${gameBoard[$index]}" | "${gameBoard[$index+1]}" | "${gameBoard[$index+2]}" |"
+			index=$(($index+3))
+	done
 
-	echo "--- --- ---"
-	echo "| ${board[1]} | ${board[2]} | ${board[3]} |"
-
-	echo "--- --- ---"
-	echo "| ${board[4]} | ${board[5]} | ${board[6]} |"
-
-	echo "--- --- ---"
-	echo "| ${board[7]} | ${board[8]} | ${board[9]} |"
 }
 
-#TO VALIDATION
-function validation (){
-	displayBoard
-	count=0
-	read -p "Enter the number between 1 to 9 " POSSITION
-	for (( index=0; index<=$POSSITION; index++ ))
-	do
-		if [ $POSSITION -eq $index ]
-		then
-			count=1
-			break
-		else
-			count=0
-		fi
-	done
-		if [ $count -eq 1 ]
-		then
-			board[$index]=X
-			displayBoard ${board[$index]}
-		else
-			validation
-		fi
+#TO CHECK FOR DIAGONAL
+function checkForDiagonal(){
+	for (( index=1; index<2; index++ ))
+			do
+			if [[ ${gameBoard[$index]} == ${gameBoard[$index+4]} ]] && [[ ${gameBoard[$index+4]} == ${gameBoard[$index+8]} ]] && [[ ${gameBoard[$index+8]} == $playerSymbol ]]
+			then
+				displayBoard
+				echo "Game Win"
+				exit
+			elif [[ ${gameBoard[$index+2]} == ${gameBoard[$index+4]} ]] && [[ ${gameBoard[$index+4]} == ${gameBoard[$index+6]} ]] && [[ ${gameBoard[$index+6]} == $playerSymbol ]]
+			then
+				displayBoard
+				echo "Game Win"
+				exit
+			fi
+		done
+}
+
+#TO CHECK FOW ROW
+function checkForRow(){
+		for (( row=1; row<=9; row=row+3 ))
+		do
+			if [[ ${gameBoard[$row]} == ${gameBoard[$row+1]} ]] && [[ ${gameBoard[$row+1]} == ${gameBoard[$row+2]} ]] && [[ ${gameBoard[$row+2]} == $playerSymbol ]]	
+			then
+				displayBoard
+				echo "Game Win"
+				exit
+			fi
+		done
+
+	}
+#TO CHECK FOR COLUMN
+function checkForColumn(){
+		for (( column=1; column<=3; column++ ))
+		do
+			if [[ ${gameBoard[$column]} == ${gameBoard[$column+3]} ]] && [[ ${gameBoard[$column+3]} == ${gameBoard[$column+6]} ]] && [[ ${gameBoard[$column+6]} == $playerSymbol ]]
+			then
+				displayBoard
+				echo "Game win"
+				exit
+			fi
+		done
 }
 
 #TO FUNCTION CALL
-validation
+function checkForWin(){
+		checkForDiagonal
+		checkForRow
+		checkForColumn
+}
+
+#TO FUNCTION PLAY
+function startPlaying(){
+local index=1
+local minValue=0
+local maxValue=9
+local player="Player"
+
+while [[ $index -gt $minValue && $index -le $maxValue ]]
+do
+	if [ $turn == $player ]
+	then
+		displayBoard
+		echo "Enter the cell number to put your symbol"
+		read index
+					if [ ${gameBoard[$index]} -eq $index ]
+					then
+							gameBoard[$index]=$playerSymbol
+							checkForWin
+					else
+							echo "check for empty cell"
+					fi
+	fi
+done
+}
+
+#TO FUNCTION CALL
+function main(){
+	resettingBoard
+	assignLatter
+	displayBoard
+	checkForWin
+	startPlaying
+}
+main
